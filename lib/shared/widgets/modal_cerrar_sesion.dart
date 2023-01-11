0eb68a70005850ca7fe42c4ec3bb460dfd2a9ef5
Jhancarlos lenis arango <jhanklear31@gmail.com>
2023-01-11 09:33:14 -0500

    fix: corrigiendo texto eliminar cuenta

--- lib/shared/widgets/modal_cerrar_sesion.dart
+++ lib/shared/widgets/modal_cerrar_sesion.dart
@@ -144,8 +144,9 @@ modalCerrarSesion(context, size, provider) {
 }
 
 modalEliminarUsuario(context, size, provider) {
-  String mensaje = "Estas apunto de eliminar tu cuenta Pideky. Tus datos serán eliminados de la aplicación. " +
-      "¿Seguro de esta acción?.";
+  String mensaje =
+      "Estás a punto de eliminar tu cuenta en Pideky. Tus datos serán eliminados de manera permanente y no podrás realizar compras desde la aplicación. " +
+          "¿Seguro de esta acción?.";
 
   Widget _botonSeguirComprando(size) {
     return GestureDetector(
