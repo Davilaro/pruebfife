a6aad8be4e59dc4265e5a070f8182c8803c910cf
Jhancarlos Lenis Arango <jahn.lenis@celuweb.com>
2023-07-18 10:31:16 -0500

    modificando alerta en mis pesdidos

--- lib/_pideky/presentation/mis_pedidos/view/mis_pedidos.dart
+++ lib/_pideky/presentation/mis_pedidos/view/mis_pedidos.dart
@@ -1,11 +1,15 @@
 import 'package:emart/_pideky/presentation/mis_pedidos/view/widgets/body_mis_pedidos.dart';
 import 'package:emart/_pideky/presentation/mis_pedidos/view_model/mis_pedidos_view_model.dart';
 import 'package:emart/shared/widgets/top_buttons.dart';
+import 'package:emart/src/preferences/preferencias.dart';
+import 'package:emart/src/utils/alertas.dart';
 import 'package:emart/src/utils/uxcam_tagueo.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../../../src/preferences/cont_colores.dart';
+
+final prefs = new Preferencias();
 
 class MisPedidosPage extends StatefulWidget {
   @override
@@ -13,6 +17,16 @@ class MisPedidosPage extends StatefulWidget {
 }
 
 class _MisPedidosPageState extends State<MisPedidosPage> {
+  @override
+  void initState() {
+    if (prefs.usurioLogin == -1) {
+      Future.delayed(Duration(seconds: 0)).then((value) {
+        alertCustom(context);
+      });
+    }
+    super.initState();
+  }
+
   final misPedidosViewModel = Get.find<MisPedidosViewModel>();
   @override
   Widget build(BuildContext context) {
