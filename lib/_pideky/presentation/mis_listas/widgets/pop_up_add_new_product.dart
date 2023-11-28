3107d5f3bcdbd84aca0a8a72c054a74b5d1261ba
Jhancarlos Lenis Arango <jahn.lenis@celuweb.com>
2023-11-28 08:43:34 -0500

    ajuste en error de tipados

--- lib/_pideky/presentation/mis_listas/widgets/pop_up_add_new_product.dart
+++ lib/_pideky/presentation/mis_listas/widgets/pop_up_add_new_product.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class PopUpAddNewProduct extends StatelessWidget {
-  final List<ListaEncabezado> nombresListas;
+  final List nombresListas;
   final Producto producto;
   final cantidad;
 
