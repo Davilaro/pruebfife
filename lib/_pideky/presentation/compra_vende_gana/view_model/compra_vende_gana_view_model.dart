e10a019e750e8f697a86afd31b8ea7c9f866b603
Jhancarlos Lenis Arango <Jhanklear31@gmail.com>
2024-03-11 08:36:48 -0500

    ajustes en modulo compra vende gana

--- lib/_pideky/presentation/compra_vende_gana/view_model/compra_vende_gana_view_model.dart
+++ lib/_pideky/presentation/compra_vende_gana/view_model/compra_vende_gana_view_model.dart
@@ -4,15 +4,10 @@ import 'package:emart/_pideky/domain/compra_vende_gana/service/compra_vende_gana
 import 'package:emart/_pideky/domain/producto/model/producto.dart';
 import 'package:emart/_pideky/infrastructure/productos/producto_repository_sqlite.dart';
 import 'package:emart/_pideky/presentation/productos/view/detalle_producto_compra.dart';
-import 'package:emart/_pideky/presentation/productos/view_model/producto_view_model.dart';
 import 'package:emart/src/classes/producto_cambiante.dart';
 import 'package:emart/src/controllers/cambio_estado_pedido.dart';
-import 'package:emart/src/preferences/class_pedido.dart';
-import 'package:emart/src/preferences/metodo_ingresados.dart';
-import 'package:emart/src/provider/carrito_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:provider/provider.dart';
 
 class CompraVendeGanaViewModel extends GetxController {
   final CompraVendeGanaService _compraVendeGanaService =
